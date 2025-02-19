4cc258703390add399c1f9817b0d5bbcbf46ab39
cadowtin <chasdowtin@gmail.com>
2025-02-19 17:51:33 -0600

    - Attack of the 300 Ft Tall Belcorra
    - Added Animation
      - Belcorra Attacks (@ChasarooniZ)

--- scripts/module.js
+++ scripts/module.js
@@ -1,11 +1,13 @@
-import { belcorraDeath } from "./animations/Chapter 10/BelcorraDeath.js"
-import { gauntlightCollapse } from "./animations/Chapter 10/GauntlightCollapse.js"
+import { belcorraAttacks } from "./animations/BelcorraAttacks.js";
+import { belcorraDeath } from "./animations/Chapter 10/BelcorraDeath.js";
+import { gauntlightCollapse } from "./animations/Chapter 10/GauntlightCollapse.js";
 
 Hooks.on("ready", () => {
-    game.abomVaultAddons = {
-        animations: {
-            belcorraDeath: belcorraDeath,
-            gauntlightCollapse: gauntlightCollapse
-        }
-    }
-})
+  game.abomVaultAddons = {
+    animations: {
+      belcorraAttacks: belcorraAttacks,
+      belcorraDeath: belcorraDeath,
+      gauntlightCollapse: gauntlightCollapse,
+    },
+  };
+});
