ff2400f318bd64d692f7eff85e35c10af02899cc
cadowtin <chasdowtin@gmail.com>
2025-02-20 00:03:44 -0600

    Remove unused compendium

--- scripts/animations/helpers.js
+++ scripts/animations/helpers.js
@@ -1,3 +1,7 @@
 export function jb2aVersion() {
-    return game.modules.get("jb2a_patreon")?.active ? 'patreon' : (game.modules.get("JB2A_DnD5e")?.active ? 'free' : 'none');
+  return game.modules.get("jb2a_patreon")?.active
+    ? "patreon"
+    : game.modules.get("JB2A_DnD5e")?.active
+    ? "free"
+    : "none";
 }
