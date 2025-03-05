12578f33ca87d1e25d73e68d816c39659501166e
cadowtin <chasdowtin@gmail.com>
2025-03-05 02:40:52 -0600

    revert

--- scripts/sceneSetup.js
+++ scripts/sceneSetup.js
@@ -1,5 +1,9 @@
-const localizeText = (str, options = {}) =>
-  game.i18n.format(`abomination-vaults-addons.drawing-text.${str}`, options);
+function localizeText(str, options = {}) {
+  return game.i18n.format(
+    `abomination-vaults-addons.drawing-text.${str}`,
+    options
+  );
+}
 
 async function setupTextandPercent(drawings, floor, pct, maxPct) {
   const increment = maxPct / drawings.length;
@@ -7,7 +11,7 @@ async function setupTextandPercent(drawings, floor, pct, maxPct) {
     pct += increment;
     SceneNavigation.displayProgressBar({
       label: localizeText("progress-text"),
-      pct,
+      pct: pct,
     });
     await setupText(drawData, floor);
   }
@@ -16,6 +20,7 @@ async function setupTextandPercent(drawings, floor, pct, maxPct) {
 
 export async function setupAllHeightTextDialog() {
   const floors = ["A", "B", "D", "E", "F", "G", "H", "J"];
+
   const content = floors
     .map(
       (floor) => `
@@ -31,7 +36,11 @@ export async function setupAllHeightTextDialog() {
 
   new Dialog({
     title: localizeText("title"),
-    content: `<form>${content}</form>`,
+    content: `
+      <form>
+        ${content}
+      </form>
+    `,
     buttons: {
       submit: {
         icon: '<i class="fas fa-check"></i>',
@@ -48,15 +57,21 @@ export async function setupAllHeightTextDialog() {
 }
 
 async function processSelectedFloors(html, floors) {
-  const floorToRun = floors.filter(
-    (floor) => html.find(`[name="floor${floor}"]`)[0].checked
-  );
-  const pctPerRoom = 100 / floorToRun.length;
-
   let pct = 0;
-  for (const floor of floorToRun) {
-    pct = await window[`addTextForFloor${floor}`](pct, pctPerRoom);
+  const floorToRun = [];
+  for (const floor of floors) {
+    const isChecked = html.find(`[name="floor${floor}"]`)[0].checked;
+    if (isChecked) floorToRun.push(floor);
   }
+  const pctPerRoom = 100 / floorToRun;
+  pct = addTextForFloorA(pct, pctPerRoom);
+  pct = addTextForFloorB(pct, pctPerRoom);
+  pct = addTextForFloorD(pct, pctPerRoom);
+  pct = addTextForFloorE(pct, pctPerRoom);
+  pct = addTextForFloorF(pct, pctPerRoom);
+  pct = addTextForFloorG(pct, pctPerRoom);
+  pct = addTextForFloorH(pct, pctPerRoom);
+  pct = addTextForFloorJ(pct, pctPerRoom);
 }
 
 export async function addTextForFloorA(pct = 0, maxPct = 100) {
