db80eb336925b16732a90dcd5e8572680adbbca2
cadowtin <chasdowtin@gmail.com>
2026-03-06 01:07:35 -0600

    2.0.0
    - **New**
      - Module updated to newest Abomination Vaults release and generally requires version `4.0.1` or higher
      - `Murmur, Implement of Empty Death` - A rework of Murmur (🧠 @weepingminotaur)
      - Added new setting to receive a reminder message to import an update
    - **Updated**
      - Fixed bug with Floor Helper Drawings setup (🐛 @reins864)
      - Fixed issue with Monk's Active Tile Triggers art of the downhill map (🐛 @reins864)

--- scripts/animations/helpers.js
+++ scripts/animations/helpers.js
@@ -1,45 +1,57 @@
 export function jb2aVersion() {
-	return game.modules.get("jb2a_patreon")?.active
-		? "patreon"
-		: game.modules.get("JB2A_DnD5e")?.active
-			? "free"
-			: "none";
+  return game.modules.get("jb2a_patreon")?.active
+    ? "patreon"
+    : game.modules.get("JB2A_DnD5e")?.active
+      ? "free"
+      : "none";
 }
 
 export function hasSequencer() {
-	return game.modules.has("sequencer");
+  return game.modules.has("sequencer");
 }
 
 export function hasPF2eGraphics() {
-	return game.modules.has("pf2e-graphics");
+  return game.modules.has("pf2e-graphics");
 }
 
 const MODULE_ID_TO_NAME = {
-	"jb2a": "JB2a",
-	"sequencer": "Sequencer",
-	"pf2e-graphics": "PF2e Graphics",
+  jb2a: "JB2a",
+  sequencer: "Sequencer",
+  "pf2e-graphics": "PF2e Graphics",
 };
 
 export function missingModulesError(modules) {
-	const missing = [];
-	for (const module of modules) {
-		let isPresent = false;
-		if (module === "jb2a") {
-			isPresent = jb2aVersion() !== "none";
-		} else {
-			isPresent = game.modules.has(module);
-		}
+  const missing = [];
+  for (const module of modules) {
+    let isPresent = false;
+    if (module === "jb2a") {
+      isPresent = jb2aVersion() !== "none";
+    } else {
+      isPresent = game.modules.has(module);
+    }
 
-		if (!isPresent) missing.push(MODULE_ID_TO_NAME[module] || module);
-	}
+    if (!isPresent) missing.push(MODULE_ID_TO_NAME[module] || module);
+  }
 
-	if (missing > 0) {
-		ui.notifications.error(
-			`This macro won't work as you are missing the following modules: <b>${missing.join(
-				", ",
-			)}</b>`,
-		);
-		return true;
-	}
-	return false;
+  if (missing > 0) {
+    ui.notifications.error(
+      `This macro won't work as you are missing the following modules: <b>${missing.join(
+        ", ",
+      )}</b>`,
+    );
+    return true;
+  }
+  return false;
+}
+
+export function sendUpdateMessage() {
+  game.settings.set(
+    MODULE_ID,
+    "previous-version.message",
+    game.modules.get("abomination-vaults-addons").version,
+  );
+  ChatMessage.create({
+    content: `<h4>${game.modules.get("abomination-vaults-addons").title}</h4>${game.i18n.localize("abomination-vaults-addons.update-message")}`,
+    whisper: game.users.contents.filter((u) => u.isGM),
+  });
 }
